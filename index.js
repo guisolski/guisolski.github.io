95dcdcad69a9f05de8b77dfd673173d6fbb581cc
Guilherme Solski <guigusolski@gmail.com>
2019-12-25 20:21:35 -0300

    incrementando

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-$(document).ready(function(){
-    $("#relax").click(function(){
-      $("#all").transition('slide left');
-      setTimeout(function(){window.location.href="relax/relax.html";}, 500);
-      
-    });
+function readjustment(){
+  
+  var width = $("#all").width();
+  var height = $("#all").height();
+  console.log(width);
+  console.log(height);
+  if(width > 760 && height > 150){
+    $("#addSeg").addClass("segment");
+    $("#addSeg").addClass("relax");
+  }else{
+    $("#addSeg").removeClass("segment");
+    $("#addSeg").removeClass("relax");
+  }
+}
+readjustment(); 
+$(document).ready(function () {
+  
+  $("#relax").click(function () {
+    $("#all").transition('slide left');
+    setTimeout(function () { window.location.href = "relax/relax.html"; }, 500);
+  });  
+  window.onresize = function () {
+    readjustment(); 
+  };
 });
