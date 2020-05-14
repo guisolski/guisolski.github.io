59b08bd36150306b7bd32bb649d79c9a0cc66786
Guilherme Solski Alves <guilhermesolskialves@gmail.com>
2020-05-14 18:21:13 -0300

    pwa test

---
+++ send-notification.sh
@@ -0,0 +1,1 @@
+curl --header "Authorization: key=YOUR_API_KEY_HERE" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\"registration_ids\":[\"YOUR_SUBSCRIPTION_ID_HERE\"]}"
