0385b4797e64dff112e9c078543cdac66e457848
Guilherme Solski Alves <guilhermesolskialves@gmail.com>
2020-05-14 18:57:47 -0300

    add image

--- assets/pwa.js
+++ assets/pwa.js
@@ -1,18 +1,18 @@
-  // Register Service Worker and sign up for push notifications
-  (() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./assets/service-worker.js')
+// Register Service Worker and sign up for push notifications
+(() => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./assets/service-worker.js')
       .then(serviceWorkerRegistration => {
         console.log('[Main] Service Worker registered');
         serviceWorkerRegistration.pushManager.subscribe({
           userVisibleOnly: true
-        }).then(function(subscription) {
+        }).then(function (subscription) {
           console.log('[Main] Subscribed to push notifications, endpoint:',
-              subscription.endpoint.replace('https://android.googleapis.com/gcm/send/', ''));
+            subscription.endpoint.replace('https://android.googleapis.com/gcm/send/', ''));
         });
       }).catch(err => {
         console.log('[Main]', err);
       });
-    }
-  })();
+  }
+})();
 
