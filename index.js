d544cb7c9f0bfe247787421a89589572aa2f275d
guilherme solski alves <guisolski@pop-os.localdomain>
2022-08-17 11:43:29 -0300

    improving the desing

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-function readjustment(){
-  
+function readjustment() {
+
   var width = $(window).width();
   var height = $(window).height();
 
-  if(width > 760 && height > 150){
+  if (width > 760 && height > 150) {
     $("#addSeg").addClass("segment");
     $("#addSeg").addClass("scrolling");
     $("#addSeg").addClass("rem50");
-  }else{
+  } else {
     $("#addSeg").removeClass("segment");
     $("#addSeg").removeClass("scrolling");
     $("#addSeg").addClass("rem50");
   }
 }
-readjustment(); 
+readjustment();
 $(document).ready(function () {
-  
+
   $("#relax").click(function () {
     $("#all").transition('slide left');
     setTimeout(function () { window.location.href = "relax/relax.html"; }, 500);
-  });  
+  });
   window.onresize = function () {
-    readjustment(); 
+    readjustment();
   };
   $('.overlay')
-  .visibility({
-    type   : 'fixed',
-    offset : 5 // give some space from top of screen
-  })
+    .visibility({
+      type: 'fixed',
+      offset: 5 // give some space from top of screen
+    })
+    ;
+    $("#aboutMe").click(function () {
+      $('.ui.modal')
+  .modal('show')
 ;
+    });
 });
